d94b3c76a55042d8a11a75963a4a5b725b6f26fe
Ahmad waqas <message2ahmad@gmail.com>
2019-12-11 02:10:54 +0800

    Initial Commit

---
+++ Landmark Remark/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Landmark Remark
+//
+//  Created by Ahmad Waqas on 11/12/19.
+//  Copyright © 2019 Ahmad Waqas. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
